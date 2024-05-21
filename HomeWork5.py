6f14d05db06a95ef6bd57050a8556843dfc4d77a
RocknRof1 <168718063+RocknRof1@users.noreply.github.com>
2024-05-21 14:34:27 +0300

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,13 @@
+my_list_ = ['apple', 'banana', 'orange', 'kiwi', 'lemon']
+print(my_list_)
+print(my_list_[0:1],my_list_[-1:])
+print(my_list_[2:5])
+my_list_[2] = 'lime'
+print(my_list_)
+
+my_dict_ = {'apple': 'яблоко', 'banana': 'банан', 'orange': 'апельсин',
+            'kiwi': 'киви', 'lemon': 'лимон'}
+print(my_dict_)
+my_dict_['apple'] = 'тыква'
+my_dict_['grape'] = 'виноград'
+print(my_dict_)
